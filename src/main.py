a5ae85f94252a087b82c0598a0101160d68d89be
mimarchant <mimarchtt@gmail.com>
2021-02-15 10:45:55 -0300

    se crean tablas de usuario y trabajador

--- src/main.py
+++ src/main.py
@@ -8,12 +8,13 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Trabajador
+#from trabajador import db, Trabajador
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Miguel1989@localhost:3306/proyecto_final'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
@@ -38,6 +39,71 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/user/signup', methods=['POST'])
+def create_user():
+    id = request.json.get('id')
+    email = request.json.get('email')
+    rut = request.json.get('rut')
+    name = request.json.get('name')
+    last_name = request.json.get('last_name')
+    address = request.json.get('address')
+    birth_date = request.json.get('birth_date')
+    gender = request.json.get('gender')
+    password = request.json.get('password')
+    status = request.json.get('status')
+    phone = request.json.get('phone')
+
+    user = User()
+    
+    user.id = id
+    user.email = email
+    user.rut = rut
+    user.name = name
+    user.last_name = last_name
+    user.address = address
+    user.birth_date = birth_date
+    user.gender = gender
+    user.password = password
+    user.status = status
+    user.phone = phone
+
+    user.save()
+    print(user)
+
+    return jsonify({"msg":"usuario creado"}), 200
+
+
+@app.route('/new/trabajaconnosotros', methods=['POST'])
+def create_trabajador():
+    id = request.json.get('id')
+    email = request.json.get('email')
+    rut = request.json.get('rut')
+    name = request.json.get('name')
+    last_name = request.json.get('last_name')
+    address = request.json.get('address')
+    birth_date = request.json.get('birth_date')
+    gender = request.json.get('gender')
+    password = request.json.get('password')
+    status = request.json.get('status')
+    phone = request.json.get('phone')
+
+    trabajador = Trabajador()
+    trabajador.tra_id = id
+    trabajador.tra_email = email
+    trabajador.tra_rut = rut
+    trabajador.tra_name = name
+    trabajador.tra_last_name = last_name
+    trabajador.tra_address = address
+    trabajador.tra_birth_date = birth_date
+    trabajador.tra_gender = gender
+    trabajador.tra_password = password
+    trabajador.tra_status = status
+    trabajador.tra_phone = phone
+
+    trabajador.save()
+
+    return jsonify({"msg":"trabajador creado, bienvenido/a"}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,78 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    rut = db.Column(db.String(15), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(180), unique=True, nullable=False)
+    phone = db.Column(db.String(15), unique=True, nullable=True)
+    birth_date = db.Column(db.String(15), unique=False, nullable=False)
+    gender = db.Column(db.String(10), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    status = db.Column(db.Boolean(), unique=False, nullable=True)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+       return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "last_name" : self.last_name,
+            "address": self.address,
+            "phone" : self.phone,
+            "birth_date" : self.birth_date,
+            "gender" : self.gender,
+            "status" : self.status,
+            "rut" : self.rut
+            
             # do not serialize the password, its a security breach
         }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+
+class Trabajador(db.Model):
+    __tablename__ = 'trabajadores'
+    tra_id = db.Column(db.Integer, primary_key=True)
+    tra_email = db.Column(db.String(120), unique=False, nullable=True)
+    tra_rut = db.Column(db.String(15), unique=False, nullable=False)
+    tra_name = db.Column(db.String(120), unique=False, nullable=False)
+    tra_last_name = db.Column(db.String(120), unique=False, nullable=False)
+    tra_address = db.Column(db.String(180), unique=False, nullable=False)
+    tra_phone = db.Column(db.String(15), unique=False, nullable=True)
+    tra_birth_date = db.Column(db.String(15), unique=False, nullable=False)
+    tra_gender = db.Column(db.String(10), unique=False, nullable=False)
+    tra_password = db.Column(db.String(80), unique=False, nullable=False)
+    tra_status = db.Column(db.Boolean(), unique=False, nullable=True)
+    
+
+    def __repr__(self):
+        return '<Trabajador %r>' % self.username
+
+    def serialize(self):
+        return {
+            "tra_id": self.tra_id,
+            "tra_email": self.tra_email,
+            "tra_name": self.tra_name,
+            "tra_last_name" : self.tra_last_name,
+            "tra_address": self.tra_address,
+            "tra_phone" : self.tra_phone,
+            "tra_birth_date" : self.tra_birth_date,
+            "tra_gender" : self.tra_gender,
+            "tra_status" : self.tra_status
+            
+            # do not serialize the password, its a security breach
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
